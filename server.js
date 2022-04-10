6db1f83b9bf4b6fa09da33249da15ee12ce87878
brutar <bruno.tar.gz@gmail.com>
2022-04-10 14:39:50 -0300

    Versão Teste

---
+++ server.js
@@ -0,0 +1,13 @@
+const { response } = require('express');
+const express = require('express');
+
+const app = express();
+
+app.get('/',(request,response)=>{
+    return response.json({message:'Servidor subiu'});
+})
+app.post('/teste',(request,response) =>{
+    const {name,date} = request.body;
+    return response.json({name, date});
+})
+app.listen(3333);
