4e1710475854b82f774a56ae64edc70e8ff32039
brutar <bruno.tar.gz@gmail.com>
2022-04-11 13:46:51 -0300

    Usando express

--- server.js
+++ server.js
@@ -1,41 +1,11 @@
-/* const { response } = require('express');
-const express = require('express'); */
-var http = require('http');
+const { response } = require('express');
+const express = require('express');
+/* var http = require('http'); */
 var fs = require('fs');
-/* const path = require('path'); */
+const path = require('path');
+const app = express();
 
-var server = http.createServer(function(request, response){
-    fs.readFile(__dirname + 'index.html', function(err, html){
-        response.writeHeader(200,{'Content-Type':'text/html'});
-        response.write(html);
-        response.end();
-    });
-});
-server.listen(3333, function(){
-    console.log('Executando App');
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* const app = express(); */
-
-/* app.get('/',(request,response)=>{
+app.get('/',(request,response)=>{
     if(request.url === '/'){
         fs.readFile(
             path.join(__dirname,'/','index.html'),
@@ -51,4 +21,4 @@ app.post('/teste',(request,response) =>{
     const {name,date} = request.body;
     return response.json({name, date});
 })
-app.listen(3333); */
+app.listen(3333);
